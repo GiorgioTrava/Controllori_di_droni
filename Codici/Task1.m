8dd1d5d127c1c1ffc3d09ce3890038cbdc7e599d
GiorgioTrava <giorgio1.travaglini@mail.polimi.it>
2021-06-03 13:04:32 +0200

    Update Task1.m
    tolto il x3 e ss al posto di uss

--- Codici/Task1.m
+++ Codici/Task1.m
@@ -5,15 +5,15 @@ clc, clear, close all
 %% Uncertain model state space representation
 
 % The stability derivatives of the system are implemented with the required
-% level of uncertainty, nominally three times the standard deviation on
+% level of uncertainty, the standard deviation on
 % Y_v, L_v, Y_delta, L_delta
 
-Y_v = ureal('Y_v',-0.264,'Percentage',3*4.837);
+Y_v = ureal('Y_v',-0.264,'Percentage',4.837);
 Y_p= 0; 
-L_v = ureal('L_v',-7.349,'Percentage',3*4.927);
+L_v = ureal('L_v',-7.349,'Percentage',4.927);
 L_p= 0;
-Y_delta = ureal('Y_delta',9.568,'Percentage',3*4.647);
-L_delta = ureal('L_delta',1079.339,'Percentage',3*2.762);
+Y_delta = ureal('Y_delta',9.568,'Percentage',4.647);
+L_delta = ureal('L_delta',1079.339,'Percentage',2.762);
 
 g = 9.807;
 
@@ -27,7 +27,7 @@ C = [0 1 0;
      0 0 1];
 D = [0 0]';
 
-SYS = uss(A,B,C,D);
+SYS = ss(A,B,C,D);
 
 SYS.StateName = {'v','p','phi'};
 SYS.InputName = 'delta_{lat}';
