d1d06ee8a30113393a96a8ebcb41de7b7a16b89a
GiorgioTrava <giorgio1.travaglini@mail.polimi.it>
2021-06-05 18:51:37 +0200

    Create Task3.m
    abbiamo creato i controllori e i due anelli, i parametri sono messi a caso per far funzionare il codice, dovremo poi sistemarli sulla base delle performace richieste

---
+++ Codici/Task3.m
@@ -0,0 +1,27 @@
+%% AEROSPACE CONTROL SYSTEMS %%
+
+clc, clear, close all
+
+run Task1
+run Task2
+
+%% Pid controller
+kp_p = 3;
+kp_i = 1;
+kp_d = 1;
+Tp = 1;
+
+kphi_p = 1;
+
+R_p = pid(kp_p, kp_i, kp_d, Tp);
+R_phi = pid(kphi_p, 0, 0);
+
+innerLoop = feedback(R_p * G,1,1,2,-1)
+
+outerLoop = feedback(R_phi * innerLoop,1,1,1,-1)
+
+figure()
+pzmap(outerLoop)
+
+tzero(outerLoop)
+pole(outerLoop)
