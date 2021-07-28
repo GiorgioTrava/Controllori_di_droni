f79a14650a3d2ee26c70814bc7e3dfb027bdc918
Fabrizio Sidari <fabrizio.sidari@mail.polimi.it>
2021-07-28 10:32:00 +0200

    modifica task3
    tunablePID

--- Codici/Task3.m
+++ Codici/Task3.m
@@ -6,17 +6,12 @@ run Task1
 run Task2
 
 %% Pid controller
-kp_p = 3;
-kp_i = 1;
-kp_d = 1;
-Tp = 1;
 
-kphi_p = 1;
 
-R_p = pid(kp_p, kp_i, kp_d, Tp);
-R_phi = pid(kphi_p, 0, 0);
+R_p = tunablePID('rrate','PID');
+R_phi = tunablePID('rangle','P');
 
-innerLoop = feedback(R_p * G,1,1,2,-1)
+innerLoop = feedback(R_p * G,1,2,1,-1)
 
 outerLoop = feedback(R_phi * innerLoop,1,1,1,-1)
 
