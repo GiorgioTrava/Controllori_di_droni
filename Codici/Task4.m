aefac1936206ce9dc5734b345cb31f7f18ba0829
Fabrizio Sidari <fabrizio.sidari@mail.polimi.it>
2021-08-06 10:26:55 +0200

    systune
    systune funziona, utilizzo di TuningGoal.Poles e TuningGoal.StepTracking

--- Codici/Task4.m
+++ Codici/Task4.m
@@ -12,18 +12,29 @@ run Task3
 % Wn >= 10 rad/s and damping ratio >= 0.9
 
 zeta = 0.9;
-wn = 100;
+wn = 10;
 
 numerator = wn^2;
 denominator = [ 1 , 2*zeta*wn , wn^2];
 
 sys_ref = tf(numerator,denominator);
 
-Req = TuningGoal.StepTracking('phi0','phi',sys_ref);
+HardReq = TuningGoal.Poles(0,0.9,30); %bisogna capire quale wmax lasciare, per avere un sistema sensato
+SoftReq = TuningGoal.StepTracking('phi0','phi',sys_ref);
+Options = systuneOptions('Display','final');
+[CL , fsoft , gHard] = systune(outerloop,SoftReq,HardReq,Options);
 
 
-viewGoal(Req)
-hold on
+figure(5)
 step(sys_ref)
+hold on
+step(CL(1,:))
+legend('reference system step response','tuned system response')
+
+
+figure(6)
+pzplot(CL,'r',outerloop,'g')
+legend('tuned','not tuned')
+
 
 
