0e9a28ff27577e9288ae4ada5e78e7e000ef5686
marcouni <marco.riccio@mail.polimi.it>
2021-09-09 12:20:19 +0200

    Risolto MUSSV

--- Codici/Task6.m
+++ Codici/Task6.m
@@ -3,41 +3,45 @@
 
 % robust stability with mu analysis
 
-% [M , Delta] = lftdata(CL1_unc);
-
+[M , Delta, BlkStruct] = lftdata(CL1_unc);
+szDelta = size(Delta);
+M11 = M(1:szDelta(2),1:szDelta(1));
+omega = logspace(-3,2,500);
+M11_g = frd(M11,omega);
+mubnds = mussv(M11_g,BlkStruct);
 %M = (-tf(info.W1)*R_p_c*R_phi_c*G(2) - tf(info.W1)*R_p_c*G(1))/([1,1]' + tf(info.W1)*R_p_c*G(1)/tf(info.W1) + tf(info.W1)*R_p_c*R_phi_c*G(2)/tf(info.W1);
-G_array_p = usample(SYS(1),60);
-[P_p , info_p] = ucover(G_array,SYS(1),5);
-W(1,1) = tf(info.W1);
-W(1,2) = tf(info.W2);
-F_mimo = getIOTransfer(CL1,'phi0',{'p','phi'});
-M = F_mimo*W;
-omega=logspace(-3,2,500);
-bounds=mussv(frd(M,omega),[1 0; 1 0]);
-
+% G_array_p = usample(SYS(1),60);
+% [P_p , info_p] = ucover(G_array,SYS(1),5);
+% W(1,1) = tf(info.W1);
+% W(1,2) = tf(info.W2);
+% F_mimo = getIOTransfer(CL1,'phi0',{'p','phi'});
+% M = F_mimo*W;
+% omega=logspace(-3,2,500);
+% bounds=mussv(frd(M,omega),[1 0; 1 0]);
+% 
 figure(300)
-sigma(bounds), grid
-
-
-W_strano(1,1) = tf(info.W1);
-W_strano(2,2) = tf(info.W2);
-W_strano.InputName = {'DELTA_{lat}'};
-W_strano.OutputName = {'z1','z2'};
-Delta = ultidyn('delta',[2,2]);
-Delta.InputName = {'z1','z2'};
-Delta.OutputName = {'w1','w2'};
-G1 = tf(SYS);
-G1.InputName = 'DELTA_{lat}';
-G1.OutputName = {'p','phi'};
-R_p_c.InputName = {'p'};
-R_p_c.OutputName = {'DELTA_{lat}'};
-R_phi_c.InputName = {'ephi'};
-R_phi_c.OutputName = {'p0'};
-
-sum_inner1 = sumblk{'ep = p - p0'};
-sum_unc = sumblk{'g_in = w1 + w2 + DELTA_{lat}'};
-
-M_delta = connect(R_phi_c,R_p_c,W_strano,Delta,G1,sum_inner1,sum_unc,{'w1','w2'},{'z1','z2'});
+sigma(mubnds), grid
+% 
+% 
+% W_strano(1,1) = tf(info.W1);
+% W_strano(2,2) = tf(info.W2);
+% W_strano.InputName = {'DELTA_{lat}'};
+% W_strano.OutputName = {'z1','z2'};
+% Delta = ultidyn('delta',[2,2]);
+% Delta.InputName = {'z1','z2'};
+% Delta.OutputName = {'w1','w2'};
+% G1 = tf(SYS);
+% G1.InputName = 'DELTA_{lat}';
+% G1.OutputName = {'p','phi'};
+% R_p_c.InputName = {'p'};
+% R_p_c.OutputName = {'DELTA_{lat}'};
+% R_phi_c.InputName = {'ephi'};
+% R_phi_c.OutputName = {'p0'};
+% 
+% sum_inner1 = sumblk{'ep = p - p0'};
+% sum_unc = sumblk{'g_in = w1 + w2 + DELTA_{lat}'};
+% 
+% M_delta = connect(R_phi_c,R_p_c,W_strano,Delta,G1,sum_inner1,sum_unc,{'w1','w2'},{'z1','z2'});
 
 
 
